18306df8ce237f9315816e60b097d18aaa2171fb
Leo.Lee <kidd1118@gmail.com>
2022-11-22 22:14:56 +0800

    nba api testing

--- src/services/nba_api.py
+++ src/services/nba_api.py
@@ -1,10 +1,27 @@
-from nba_api.live.nba.endpoints import scoreboard
+# import numpy as np
 
-# Today's Score Board
-games = scoreboard.ScoreBoard()
+# arr = np.array([1, 2, 3])
 
-# json
-games.get_json()
+# print(arr)
 
-# dictionary
-games.get_dict()
+# print(np.__name__)  # 模組名稱
+# print(np.__package__)  # 套件名稱
+# print(np.__file__)  # 模組的檔名及路徑
+
+from nba_api.stats.static import teams
+
+nba_teams = teams.get_teams()
+# Select the dictionary for the Celtics, which contains their team ID
+celtics = [team for team in nba_teams if team['abbreviation'] == 'BOS'][0]
+celtics_id = celtics['id']
+
+# from nba_api.live.nba.endpoints import scoreboard
+
+# # Today's Score Board
+# games = scoreboard.ScoreBoard()
+
+# # json
+# games.get_json()
+
+# # dictionary
+# games.get_dict()
